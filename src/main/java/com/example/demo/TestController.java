bcdf4511f23929066530f1645fa6d052e9b26ede
kyo_y <kyo_y@DESKTOP-9NK51U9>
2021-05-30 15:13:31 +0900

    adding html contents

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -14,10 +15,14 @@ public class TestController {
 	public ConfigurationMetadataStudyForNormalProps cmsfnp;
 	
 	@GetMapping("/testProps")
-	public String test() {
-		System.out.println("test Colors isntance : " + cmsfcp.getColors().toString());
+	public ModelAndView testProps(ModelAndView mav) {
+		System.out.println("test Colors instance : " + cmsfcp.getColors().toString());
 		System.out.println("test Colors size : " + cmsfcp.getColors().size());
 
-		return "index";
+		mav.setViewName("index");
+		mav.addObject("ColorsInstance",cmsfcp.getColors());
+		mav.addObject("ColorsSize",cmsfcp.getColors().size());
+
+		return mav;		
 	}
 }
